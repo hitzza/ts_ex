4c8f5e1dff512ee2d18cc791bce51ea686237b21
hitzza <cslzh4433@gmail.com>
2023-01-13 18:43:29 +0900

    implement typescript environment

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// index.ts
+var button = document.getElementById("button");
+button.addEventListener("click", function () {
+    alert("clicked!");
+});
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+/*
+const button = document.getElementById("button");
+button.addEventListener("click", () => {
+  alert("clicked!");
+});
+*/
+
+function sum(a:number, b:number, args:number[]){
+    let element = 0;
+    for (let index = 0; index < args.length; index++) {
+        element += args[index];
+        
+    }
+    return a+b+element;
+}
+console.log(sum(1,2,[4,5,6,7]));
